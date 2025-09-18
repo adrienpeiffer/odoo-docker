bf21231905c53416abd4b189cbbd44eeb397f421
Adrien Peiffer <adrien.peiffer@co.etat.lu>
2025-09-18 09:27:43 +0200

    Install gosu

--- 13.0/entrypoint.sh
+++ 13.0/entrypoint.sh
@@ -38,22 +38,10 @@ check_config "db_port" "$PORT"
 check_config "db_user" "$USER"
 check_config "db_password" "$PASSWORD"
 
-case "$1" in
-    -- | odoo)
-        shift
-        if [[ "$1" == "scaffold" ]] ; then
-            exec odoo "$@"
-        else
-            wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-            exec odoo "$@" "${DB_ARGS[@]}"
-        fi
-        ;;
-    -*)
-        wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-        exec odoo "$@" "${DB_ARGS[@]}"
-        ;;
-    *)
-        exec "$@"
-esac
+wait-for-psql.py ${DB_ARGS[@]} --timeout=30
 
-exit 1
+if [ -z "${NOGOSU:-}" ] ; then
+  exec gosu odoo "$@"
+else
+  exec "$@"
+fi
